f28e59e9b2e41b8d1313f3ae9559f2e4bfde8dd1
PB <p.baessler@wus.agency>
2022-03-25 15:12:03 +0100

    add screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useMachine } from '@xstate/react'
-import React from 'react'
+import React, { Children, FunctionComponent, MouseEventHandler, useContext, useEffect } from 'react'
 import './App.css'
+import { Button } from './components/Button/Button'
 import Stage from './components/Stage/Stage'
 import { AppMachine } from './machine'
 import { AppContext } from './machine/context'
@@ -10,13 +11,61 @@ import { AppContext } from './machine/context'
 const App = () => {
   const [state, send] = useMachine(AppMachine)
 
+  useEffect(() => {
+    console.log('App state:', state)
+  }, [state])
+  
+
   return (
-    <AppContext.Provider value={[state, send ]}>
+    <AppContext.Provider value={[state, send]}>
       <div className="App">
-        <Stage />
+
+        {state.value === 'idle' && <StartScreen />}
+        {state.value.breathing && <BreathScreen />}
+        {state.value.holding && <HoldingScreen />}
+        
+        <Button onClick={() => send('NEXT')}>NEXT</Button>
       </div>
     </AppContext.Provider>
   )
 }
+
+
+const StartScreen = () => {
+  return (
+    <div className="StartScreen">
+      <h1>Welcome to the app!</h1>
+    </div>
+  )
+}
+
+
+const BreathScreen = () => {
+
+  const [state, send] = useContext(AppContext)
+
+  return (
+    <div className="StartScreen">
+      <h1>Breath {state.context.breaths} / {state.context.maxBreaths} </h1>
+    </div>
+  )
+}
+
+const HoldingScreen = () => {
+  const [state, send] = useContext(AppContext)
+
+  useEffect(() => {
+    const onDbClick = () => send('NEXT')
+    window.addEventListener('dblclick' , onDbClick)
+    return () => window.removeEventListener('dblclick', onDbClick)
+  }, [])
+
+  return (
+    <div className="HoldingScreen">
+      <h1>{state.value.holding}</h1>
+    </div>
+  )
+}
+
 
 export default App
