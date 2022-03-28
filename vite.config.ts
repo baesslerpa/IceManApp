97bd42c89c8bc421d586265fdbdb30dee5011d16
PB <p.baessler@wus.agency>
2022-03-28 19:39:30 +0200

    add favicon and robots

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,34 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    VitePWA({}),
-    react()
-  ]
+    VitePWA({
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        name: 'IceMan Breath App',
+        short_name: 'Breath App',
+        description: 'App to practice Breathing',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        theme_color: '#28545f',
+        background_color: '#28545f',
+        display: 'standalone',
+      },
+    }),
+    react(),
+  ],
 })
