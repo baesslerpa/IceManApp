081e6a1b758b5760977eb64af9cdc1fbfaac6347
PB <p.baessler@wus.agency>
2022-04-03 10:32:31 +0200

    fix favicon

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,17 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     VitePWA({
+      devOptions: {
+        enabled: true
+        /* other options */  
+      },
       includeAssets: [
         'favicon.svg',
         'favicon.ico',
         'robots.txt',
         'apple-touch-icon.png',
+        'android-chrome-192x192.png',
+        'assets/android-chrome-512x512.png'
       ],
       manifest: {
         name: 'IceMan Breath App',
@@ -18,12 +24,12 @@ export default defineConfig({
         description: 'App to practice Breathing',
         icons: [
           {
-            src: '/android-chrome-192x192.png',
+            src: 'assets/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/android-chrome-512x512.png',
+            src: 'assets/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
